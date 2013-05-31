dbb53d056dd909eb2b6c5c23afe2f0761af76fa3
Martin Dengler <martin@martindengler.com>
2013-05-31 22:22:06 +0100

    import existing emacs site-lisp

---
+++ .emacs.d/site-lisp/org.el
@@ -0,0 +1,70 @@
+(require 'org-install)
+
+
+
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+
+
+
+(define-key mode-specific-map [?a] 'org-agenda)
+
+
+
+(eval-after-load "org"
+
+  '(progn
+
+     (define-prefix-command 'org-todo-state-map)
+
+
+
+     (define-key org-mode-map "\C-cx" 'org-todo-state-map)
+
+
+
+     (define-key org-todo-state-map "x"
+
+       #'(lambda nil (interactive) (org-todo "CANCELLED")))
+
+     (define-key org-todo-state-map "d"
+
+       #'(lambda nil (interactive) (org-todo "DONE")))
+
+     (define-key org-todo-state-map "f"
+
+       #'(lambda nil (interactive) (org-todo "DEFERRED")))
+
+     (define-key org-todo-state-map "l"
+
+       #'(lambda nil (interactive) (org-todo "DELEGATED")))
+
+     (define-key org-todo-state-map "s"
+
+       #'(lambda nil (interactive) (org-todo "STARTED")))
+
+     (define-key org-todo-state-map "w"
+
+       #'(lambda nil (interactive) (org-todo "WAITING")))
+
+
+
+     (define-key org-agenda-mode-map "\C-n" 'next-line)
+
+     (define-key org-agenda-keymap "\C-n" 'next-line)
+
+     (define-key org-agenda-mode-map "\C-p" 'previous-line)
+
+     (define-key org-agenda-keymap "\C-p" 'previous-line)))
+
+
+
+(require 'remember)
+
+
+
+(add-hook 'remember-mode-hook 'org-remember-apply-template)
+
+
+
+(define-key global-map [(control meta ?r)] 'remember)
+
