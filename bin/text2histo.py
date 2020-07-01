409beb82fb8ad0b1077cd868e34e262b6c456af5
Martin Dengler <martin@martindengler.com>
2020-07-01 11:13:58 +0000

    text2histo.py: black

--- bin/text2histo.py
+++ bin/text2histo.py
@@ -102,7 +102,6 @@ import os
 import sys
 
 
-
 def get_counts(lines):
     """returns a dict of integer -> occurrence count"""
 
@@ -132,7 +131,7 @@ def get_bin_contents(counts, bins=None):
     if bins is None:
         bins = min(20, len(counts.keys()))
     else:
-        bins = int(bins) # in case it's a string
+        bins = int(bins)  # in case it's a string
 
     bin_range = (max_c - min_c) + 1
     bin_increment = bin_range / math.ceil(float(bins))
@@ -149,10 +148,13 @@ def get_bin_contents(counts, bins=None):
         else:
             bin_end = bin_start - bin_increment
 
-        size = sum([counts[c] for c in counts
-                    if ((c <= bin_start)
-                        and
-                        ((c > bin_end) or (last_bin and c == bin_end)))])
+        size = sum(
+            [
+                counts[c]
+                for c in counts
+                if ((c <= bin_start) and ((c > bin_end) or (last_bin and c == bin_end)))
+            ]
+        )
 
         bin_contents.append([bin_start, bin_end, size])
         bin_start = bin_end
@@ -169,26 +171,30 @@ def print_histogram_bins(bin_contents, max_bar_width=None):
 
     width_normalizer = width / float(max([size for s, e, size in bin_contents]))
 
-    bin_start_and_ends = reduce(lambda a, b: a + b,
-                                [[s, e] for s, e, size in bin_contents], [])
+    bin_start_and_ends = reduce(
+        lambda a, b: a + b, [[s, e] for s, e, size in bin_contents], []
+    )
     bin_label_padding = max(map(len, map(str, bin_start_and_ends))) + 1
     size_padding = max([len(str(size)) for s, e, size in bin_contents])
-    format_str = "%%%ds ..%%%ds | [%%%ds] %%s\n" % (bin_label_padding,
-                                                    bin_label_padding,
-                                                    size_padding)
+    format_str = "%%%ds ..%%%ds | [%%%ds] %%s\n" % (
+        bin_label_padding,
+        bin_label_padding,
+        size_padding,
+    )
 
     for bin_start, bin_end, size in bin_contents:
-        sys.stdout.write(format_str % (bin_start,
-                                       bin_end,
-                                       size,
-                                       "#" * int(size * width_normalizer)))
+        sys.stdout.write(
+            format_str % (bin_start, bin_end, size, "#" * int(size * width_normalizer))
+        )
+
 
 if __name__ == "__main__":
 
     parser = optparse.OptionParser()
     parser.add_option("--bins", default=None)
-    parser.add_option("-r", "--raw", action="store_true",
-                      help="print raw scatter-plot-suitable data")
+    parser.add_option(
+        "-r", "--raw", action="store_true", help="print raw scatter-plot-suitable data"
+    )
     parser.add_option("-w", "--max-bar-width", default=None)
     options, args = parser.parse_args()
 
@@ -202,7 +208,7 @@ if __name__ == "__main__":
     bin_contents = get_bin_contents(counts, bins=options.bins)
 
     if options.raw:
-        for bin_high, bin_low, bin_value in bin_contents: #destructure for clarity
+        for bin_high, bin_low, bin_value in bin_contents:  # destructure for clarity
             print ",".join(map(str, (bin_high, bin_low, bin_value)))
     else:
         print_histogram_bins(bin_contents, max_bar_width=options.max_bar_width)
