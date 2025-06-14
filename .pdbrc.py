e8dcf5e4d60d3e7aa5147e05d7a9b6048aa228ba
Martin Dengler <martin@martindengler.com>
2025-06-13 21:53:24 -0400

    .pdbrc.py: fix exponential-history-increase bug

--- .pdbrc.py
+++ .pdbrc.py
@@ -1,3 +1,4 @@
+
 _pdbrc_read = None
 def _pdbrc_init():
     global _pdbrc_read
@@ -6,12 +7,22 @@ def _pdbrc_init():
     # Save history across sessions
     import os, readline
     histfile = os.path.expanduser("~/.pdb-pyhist")
+    prev_h_len = None
     try:
         readline.read_history_file(histfile)
+        h_len = readline.get_current_history_length()
     except IOError:
         pass
+    def save(prev_h_len, histfile):
+        if not os.path.exists(histfile):
+            readline.write_history_file(histfile)
+        else:
+            save_h_len = readline.get_current_history_length()
+            if prev_h_len is not None:
+                save_h_len -= prev_h_len
+            readline.append_history_file(save_h_len, histfile)
     import atexit
-    atexit.register(readline.write_history_file, histfile)
+    atexit.register(save, prev_h_len, histfile)
     readline.set_history_length(-1)
     _pdbrc_read = True
 
